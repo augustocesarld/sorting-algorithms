f234eec3f60093550bcc86696db118fca1d2210d
Augusto <augustocesar827@gmail.com>
2019-06-04 14:58:57 -0300

    Useless files removed

--- verifier.js
+++
@@ -1,8 +0,0 @@
-function verifyArray(array, crescent = true) {
-    for (let i = 1; i < array.length; i++)
-        if ((crescent && array[i] < array[i-1]) || (!crescent && array[i] > array[i-1]))
-            return false
-    return true
-}
-
-module.exports = {verifyArray}
